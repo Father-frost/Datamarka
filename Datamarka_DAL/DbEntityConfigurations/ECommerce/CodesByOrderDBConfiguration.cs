9f8ba4011dabdf0eb9ed2a605fd45bc016ec0dec
Александр Коляда <aleksander.kolyada@gmail.com>
2024-07-28 22:08:29 +0300

    Add DbEntityConfigurations for migrations creating.

---
+++ Datamarka_DAL/DbEntityConfigurations/ECommerce/CodesByOrderDBConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Datamarka_DomainModel.Models.ECommerce;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Datamarka_DAL.DbEntityConfigurations.ECommerce
+{
+    internal class CodesByOrderDBConfiguration : IEntityTypeConfiguration<CodesByOrder>
+    {
+        public void Configure(EntityTypeBuilder<CodesByOrder> builder)
+        {
+
+        }
+    }
+}
---
+++ Datamarka_DAL/DbEntityConfigurations/ECommerce/ProductDBConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Datamarka_DomainModel.Models.ECommerce;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Datamarka_DAL.DbEntityConfigurations.ECommerce
+{
+    internal class ProductDBConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+
+        }
+    }
+}
